c92ec2c36bcf59e687d389a043d7e0fe1b80d6b7
Abhinav <abhiasthana76@gmail.com>
2024-06-28 14:17:12 +0530

    Player and computer moves

--- main.py
+++ main.py
@@ -1,18 +1,22 @@
-# import random
+import random
 # import time 
 # import array
 import numpy as np 
 
+# Assigning symbols to each player globally
 PLAYER = 'X'
 COMPUTER = 'O'
 
+# setting up board using numpy 2d array
 board = np.zeros([3, 3], dtype=str)
 
+# assigning empty blocks on board
 def setBoard():
     for i in range(0, 3):
         for j in range(0, 3):
-            board[i, j] = ' '
+            board[i][j] = ' '
 
+# printing board
 def printBoard():
     print(f"{board[0][0]}  |  {board[0][1]}| {board[0][2]}")
     print("---|---|---")
@@ -23,17 +27,45 @@ def printBoard():
     print(f"{board[2][0]}  |  {board[2][1]}| {board[2][2]}")
     print()
 
+# checking if there's any free space on the board
 def checkfreeSpace():
     emptyblock = 9
     for i in range(3):
         for j in range(3):
-            if board[i][j] == ' ':
+            if board[i, j] != ' ':
                 emptyblock -= 1
-                
+    print(f"Emptyblocks left: {emptyblock}")
+    return(emptyblock)
+
+# Player moves on the board
 def playerMove():
-    pass
+    r = int(input("Enter the row #0-3: "))
+    c = int(input("Enter the columun #0-3: "))
+    if board[r][c] != ' ':
+        print("Invalid Move, Try again: ")
+
+    while board[r][c] == ' ':
+            board[r][c] = PLAYER
+            break
 def computerMove():
-    pass
+    
+    print("computer turn: ")
+    r = random.randint(0, 2)
+    print(f"Row: {r}")
+    c = random.randint(0, 2)
+    print(f"Column: {c}")
+    
+    if checkfreeSpace() >  0:
+        if board[r][c] != ' ':
+            print("Invalid move: ")
+            computerMove()
+
+        else: 
+            while board[r][c] == ' ':
+                board[r][c] = COMPUTER
+                break
+    else:
+        print("w")
 def checkWinner():
     pass
 def printWinner():
@@ -41,8 +73,10 @@ def printWinner():
 
 if __name__ == "__main__":
     setBoard()
-    printBoard()
-
+    while checkfreeSpace() != 0:
+        printBoard()
+        playerMove()
+        computerMove()
 
 
 
