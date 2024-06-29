b765b146457743cd965b932356878720762f20ba
Abhinav <abhiasthana76@gmail.com>
2024-06-29 11:10:55 +0530

    Main changes

--- main.py
+++ main.py
@@ -6,7 +6,7 @@ import numpy as np
 # Assigning symbols to each player globally
 PLAYER = 'X'
 COMPUTER = 'O'
-
+WINNER = ' '
 # setting up board using numpy 2d array
 board = np.zeros([3, 3], dtype=str)
 
@@ -66,17 +66,46 @@ def computerMove():
                 break
     else:
         print("w")
-def checkWinner():
-    pass
-def printWinner():
-    pass
 
+def checkWinner():
+    # Checking rows
+    for i in range(3):
+        if board[i][0] == board[i][1] and board[i][0] == board[i][2]:
+            print(f"Winner: {board[i][0]}")
+            return(board[i][0])
+    
+    # Checking column
+    for j in range(3):
+        if board[0][j] == board[1][j] and board[0][j] == board[2][j]:
+            print(f"Winner: {board[0][j]}")
+            return(board[0][j])
+    
+    # Checking Diagonals
+    if board[0][0] == board[1][1] and board[0][0] == board[2][2]:
+        print(f"Winner: {board[0][0]}")
+        return(board[0][0])
+    if board[2][2] == board[1][1] and board[2][2] == board[2][0]:
+        print(f"Winner: {board[2][2]}")
+        return(board[2][2])
+
+def printWinner(WINNER):
+    if WINNER == PLAYER:
+        print("YOU WIN!\n")
+    elif WINNER == COMPUTER:
+        print("YOU LOSE!\n")
+    else:
+        print("IT'S A TIE.")
+
+# Main
 if __name__ == "__main__":
     setBoard()
-    while checkfreeSpace() != 0:
+    while WINNER == ' ' or checkfreeSpace() != 0:
         printBoard()
         playerMove()
         computerMove()
-
-
+        WINNER = checkWinner()
+        if WINNER != ' ' or checkfreeSpace() == 1:
+            printWinner(WINNER)
+            break
+        
 
