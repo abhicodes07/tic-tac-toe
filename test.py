071e97963961986385c986617065d0610d19df2d
Abhinav <abhiasthana76@gmail.com>
2024-10-14 11:15:41 +0530

    update

--- test.py
+++ test.py
@@ -1,9 +1,50 @@
 import numpy as np 
+# from time import sleep
+
+class Players:
+    def __init__(self, p):
+        self.name = name
+        self.choice = " "
+        self.ponts = 0
+
+    def choose(self):
+        pass
+    
+    def incrementPoints(self):
+        pass
+
+class GameRound:
+    def __init__(self):
+        pass
+
+    def freeSpace(self):
+        pass
 
 
-PLAYER = 'X'
-board = np.zeros((3, 3), dtype=str)
-for i in range(3):
-    for j in range(3):
-        board[i][j] = '*'
-print(board)
+class StartGame:
+    def __init__(self):
+        self
+        self.board = np.zeros([3, 3], dtype=str)
+
+    def setBoard(self):
+        for i in range(0, 3):
+            for j in range(0, 3):
+                self.board[i][j] = ' '
+
+    def printtBoard(self):
+        print(f"{self.board[0][0]}  | {self.board[0][1]} | {self.board[0][2]}")
+        print("---|---|---")
+
+        print(f"{self.board[1][0]}  | {self.board[1][1]} | {self.board[1][2]}")
+        print("---|---|---")
+
+        print(f"{self.board[2][0]}  | {self.board[2][1]} | {self.board[2][2]}")
+        print()
+
+
+    def printWinner(self):
+        pass
+
+if __name__ == "__main__":
+    pass
+
